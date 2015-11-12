f51cf7f7e4589a8da9f091434f487dbfd4b0f05a
Brett Warner <brett@warbrett.com>
2015-11-11 22:13:08 -0700

    cleanup for npm publish

--- src/lib/rest-client.js
+++ src/lib/rest-client.js
@@ -1,8 +1,11 @@
 'use strict';
 
 import rest from 'rest';
+import mime from 'rest/interceptor/mime';
 import errorCode from 'rest/interceptor/errorCode';
 
-const restClient = rest.wrap(errorCode);
+const client = rest
+  .wrap(mime, { mime: 'application/x-www-form-urlencoded' })
+  .wrap(errorCode);
 
-export default restClient;
+export default client;
