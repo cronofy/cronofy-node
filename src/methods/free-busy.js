56873f74b4c213eaf0244e0c834b049ac7193f0e
Jared Windover <jaredwindover@gmail.com>
2017-03-19 15:30:10 -0400

    Adding next_page option to freebusy

--- src/methods/free-busy.js
+++ src/methods/free-busy.js
@@ -9,11 +9,11 @@ import rest from '../lib/rest-client';
 function freeBusy (options, callback) {
   const settings = {
     method: 'GET',
-    path: options.urls.api + '/v1/free_busy',
+    path: options.next_page || options.urls.api + '/v1/free_busy',
     headers: {
       Authorization: 'Bearer ' + options.access_token
     },
-    params: _.omit(options, 'access_token')
+    params: _.omit(options, 'access_token', 'next_page')
   };
   const result = rest(settings).fold(reach, 'entity');
 
