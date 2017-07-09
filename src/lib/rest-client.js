b48053363b0c18ba1f56a91e4c52705c1e191d72
jleroy <julien.leroy@iwecloud.com>
2017-07-09 19:59:24 +0200

    Fix GET requests sent with queryParams instead of entity

--- src/lib/rest-client.js
+++ src/lib/rest-client.js
@@ -3,9 +3,11 @@
 var rest = require('rest');
 var defaultRequest = require('rest/interceptor/defaultRequest');
 var mime = require('rest/interceptor/mime');
+var params = require('rest/interceptor/params');
 var errorCode = require('rest/interceptor/errorCode');
 
 var client = rest
+  .wrap(params)
   .wrap(defaultRequest, { headers: { 'User-Agent': 'Cronofy Node' } })
   .wrap(mime, { mime: 'application/json' })
   .wrap(errorCode);
